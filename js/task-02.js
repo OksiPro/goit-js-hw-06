bac765e2a94c26fc3dfc8f3d9bbf9cc7c69a6bdc
OksiPro <oksi15pro@gmail.com>
2023-08-29 22:09:42 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const container = document.querySelector(`#ingredients`);
+
+const liArray = ingredients.map((ingredient) => {
+  return `<li class="item">
+  ${ingredient}
+  </li>`
+});
+
+const markup = liArray.join('');
+container.insertAdjacentHTML(`afterbegin`, markup);
+
+console.log(markup);
