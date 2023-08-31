bb41c9bee3af3e620f3b93e191ce90fa56184e7c
OksiPro <oksi15pro@gmail.com>
2023-08-31 20:01:33 +0300

    add8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector('.login-form');
 
+    loginForm.addEventListener('submit', function (event) {
+      event.preventDefault();
+
+      const formData = new FormData(loginForm);
+
+      const formDataObject = {};
+        
+      formData.forEach((value, key) => {
+        formDataObject[key] = value;
+      });
+
+      if (!formDataObject.email || !formDataObject.password) {
+        alert('Всі поля повинні бути заповнені');
+      } else {
+        console.log(formDataObject); 
+        loginForm.reset();
+      }
+    });
