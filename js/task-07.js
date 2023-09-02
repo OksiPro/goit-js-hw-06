c781239a6a10728f81dd6a9ac9903a34b03e0472
OksiPro <oksi15pro@gmail.com>
2023-09-02 17:08:06 +0300

    corrected(7)

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,10 @@
 const inputControl = document.querySelector('#font-size-control');
 const spanText = document.querySelector('#text');
 
+const initialFontSize = getComputedStyle(spanText).fontSize;
+
+inputControl.value = parseInt(initialFontSize);
+
 inputControl.addEventListener('input', () => {
     const fontSize = inputControl.value + 'px'; 
     spanText.style.fontSize = fontSize;
